0e1820da04f9d57ec97571ed814993849ee5671e
pratibha-tech-cloud <pratibha.yadav.tech@gmail.com>
2026-08-19 17:08:07 +0530

    This is my first commit.

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "d0200003-d736-4f7e-aeba-1c554da49b47"
+}
